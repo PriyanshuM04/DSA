c17dc394fbf5f936ce4d2d20eabfa7ff73714e41
PriyanshuM04 <priyanshumallick04@gmail.com>
2024-06-28 13:13:20 +0530

    Update DataStructres

--- OOPSinJAVA/Polymorphism.java
+++
@@ -1,8 +0,0 @@
-package OOPSinJAVA;
-
-public class Polymorphism {
-    public static void main(String[] args) {
-        Student s1 = new Student("Raj", 04, 80.1);
-        s1.printInfo(s1.name, s1.roll);
-    }
-}
--- Questions/Trial.java
+++
@@ -1,8 +0,0 @@
-package Questions;
-
-public class Trial {
-    public static void main(String[] args) {
-        int i = (int)((5+0)/2);
-        System.out.println(i);
-    }
-}
