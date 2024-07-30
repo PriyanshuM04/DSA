18bc41006235572bdedd89516bb6915535f83c23
PriyanshuM04 <priyanshumallick04@gmail.com>
2024-07-30 09:48:12 +0530

    Added OOPs

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,8 @@
+package OOPSinJAVA;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Student s1 = new Student("Raj", 04, 80.1);
+        s1.printInfo(s1.name, s1.roll);
+    }
+}
