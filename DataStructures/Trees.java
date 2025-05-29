1cc0929fb71ff073c4b55941e3670dec3bf37dcd
PriyanshuM04 <priyanshumallick04@gmail.com>
2025-05-29 18:49:36 +0530

    Tree Traversal

--- DataStructures/Trees.java
+++ DataStructures/Trees.java
@@ -1,5 +1,7 @@
 package DataStructures;
 
+import java.util.*;
+
 public class Trees {
     static class Node {
         int data;
@@ -26,11 +28,67 @@ public class Trees {
         }
     }
 
+    // Tree Traversal
+    public static void preOrder(Node root) {
+        if (root==null) {
+//            System.out.print(-1 + " ");
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void inOrder(Node root) {
+        if (root==null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+    public static void postOrder(Node root) {
+        if (root==null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode==null) {
+                System.out.println();
+                if (q.isEmpty()) break;
+                else q.add(null);
+            }
+            else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // PreOrder
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+//        System.out.println(root.data);
+//        preOrder(root);
+//        inOrder(root);
+//        postOrder(root);
+        levelOrder(root);
     }
 }
