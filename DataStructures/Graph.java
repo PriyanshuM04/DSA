e0d6ed1426f160b4929f6e568896e653b7466c78
PriyanshuM04 <priyanshumallick04@gmail.com>
2025-06-28 19:36:16 +0530

    Create Graph

---
+++ DataStructures/Graph.java
@@ -0,0 +1,78 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class Graph {
+    static class  Pair {
+        int u, v;
+        Pair (int u, int v) {
+            this.u = u;
+            this.v = v;
+        }
+    }
+
+    static void printMat(int[][] mat) {
+        int n = mat.length;
+        for (int i=1; i<n; i++) {
+            for (int j=1; j<n; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] createMat(int n, int m, Pair[] edges) {
+        int[][] adj = new int[n+1][n+1];
+
+        for (int i=0; i<m; i++) {
+            int u = edges[i].u;
+            int v = edges[i].v;
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+        }
+
+        return adj;
+    }
+
+    static void printAdjList(ArrayList<ArrayList<Integer>> adj) {
+        int n = adj.size();
+        for (int i=0; i<n; i++) {
+            System.out.print(i + "->");
+            for (int node : adj.get(i)) {
+                System.out.print(node + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> listGraph(int n, int m, Pair[] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i=0; i<=n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i=0; i<m; i++) {
+            int u = edges[i].u;
+            int v = edges[i].v;
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        return adj;
+    }
+
+    public static void main(String[] args) {
+        int n = 5, m = 4;
+        Pair[] edges = new Pair[m];
+        edges[0] = new Pair(1, 2);
+        edges[1] = new Pair(1, 3);
+        edges[2] = new Pair(2, 4);
+        edges[3] = new Pair(4, 5);
+
+//        int[][] mat = createMat(n, m, edges);
+//        printMat(mat);
+
+        ArrayList<ArrayList<Integer>> graph = listGraph(n, m, edges);
+        printAdjList(graph);
+    }
+}
